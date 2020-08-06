478b6a6fd91b7070775745933a4d78946505d8ba
andrew stebenne <andrew@aedit.com>
2020-08-06 14:24:34 -0400

    update color page for new palette attrs

--- pages/color.js
+++ pages/color.js
@@ -9,47 +9,61 @@ const ColorPage = () => (
 
     <h1>{'Color Demo (hacked by Andrew)'}</h1>
 
+    <p>
+      {'These divs have a'}
+      <code>{'[data-pal]'}</code>
+      {'attribute to select a palette.'}
+    </p>
     <section className="color-demo">
       {['q', 'w', 'e', 'r', 't'].map((shadeIdx, idx) => (
-        <div key={`color-demo-${shadeIdx}`}>
-          <span>{`--${shadeIdx}-shade`}</span>
-          <span>{`(text: --${shadeIdx}-text)`}</span>
+        <div key={shadeIdx} data-pal={shadeIdx}>
+          <span>{`fill: --${shadeIdx}-shade`}</span>
+          <span>{`text: --${shadeIdx}-text`}</span>
+        </div>
+      ))}
+    </section>
+
+    <p>
+      {'These divs have the same'}
+      <code>{'data-pal'}</code>
+      {'values, and an additional'}
+      <code>{'[data-pal-scheme="invert"]'}</code>
+      {'to control how the palette is used.'}
+    </p>
+
+    <section className="color-demo">
+      {['q', 'w', 'e', 'r', 't'].map((shadeIdx, idx) => (
+        <div key={shadeIdx} data-pal={shadeIdx} data-pal-scheme="invert">
+          <span>{`fill: --${shadeIdx}-text`}</span>
+          <span>{`text: --${shadeIdx}-shade`}</span>
         </div>
       ))}
     </section>
 
     <pre>
       <code className="scss">{`
-$shaderefs: [ 'q' 1, 'w' 2, 'e' 3, 'r' 4, 't' 5, ];
-
 .color-page {
-  pre {
-    width: 100%;
-
-    code {
-      margin: var(--s2) var(--s1);
-    }
+  > * + * {
+    margin-top: var(--s1);
   }
 
   section.color-demo {
     display: flex;
-
     justify-content: space-evenly;
-
     padding-top: var(--s3);
     padding-bottom: var(--s3);
+    border: 1px solid black;
+    margin-left: var(--s3);
+    margin-right: var(--s3);
 
-    & > div {
-      width: var(--s4);
-      height: var(--s4);
-
-      content: ' ';
-
-      @each $ref, $index in $shaderefs {
-        &:nth-child(#{$index}) {
-          background: var(--#{$ref}-shade);
-        }
-      }
+    > div {
+      width: var(--s5);
+      height: var(--s5);
+      display: flex;
+      flex-direction: column;
+      place-content: center center;
+      text-align: center;
+      font-size: large;
     }
   }
 }
