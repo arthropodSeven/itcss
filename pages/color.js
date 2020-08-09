b59da3469814545d0c139841d248583f5eea2e66
andrew stebenne <andrew@aedit.com>
2020-08-09 17:32:33 -0400

    update color page

--- pages/color.js
+++ pages/color.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Highlighter from 'react-syntax-highlighter'
+import { paraisoDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const ColorPage = () => (
   <article className="color-page" id="home">
@@ -8,6 +10,14 @@ const ColorPage = () => (
     </Head>
 
     <h1>{'Color Demo (hacked by Andrew)'}</h1>
+
+    <p>
+      {"Since this is a prototype, I've restricted myself to monochrome "}
+      {"colorschemes. It's convenient because I can use sass builtins to "}
+      {'derive secondary colors: '}
+      <a href="#generating-colors">{'See below'}</a>
+      {'.'}
+    </p>
 
     <p>
       {'These divs have a'}
@@ -42,8 +52,7 @@ const ColorPage = () => (
       ))}
     </section>
 
-    <pre>
-      <code className="scss">{`
+    <Highlighter language="scss" style={paraisoDark}>{`
 .color-page {
   > * + * {
     margin-top: var(--s1);
@@ -69,9 +78,72 @@ const ColorPage = () => (
     }
   }
 }
-    `}</code>
-    </pre>
-    <div></div>
+    `}</Highlighter>
+
+    <h2 id="generating-colors">{'Generating Colors'}</h2>
+
+    <p>
+      {'See below for the code that generates these colorschemes from a '}
+      {'basic monochrome palette.'}
+    </p>
+
+    <Highlighter
+      language="scss"
+      style={paraisoDark}
+      customStyle={{ overflowX: 'none' }}>{`
+@use "sass:color";
+@use "sass:math";
+
+$shades: 'q' 1 'russian-violet' hsla(255, 45%, 18%, 1),
+  'w' 2 'purple-navy' hsla(250, 26%, 44%, 1),
+  'e' 3 'purple-mountain-majesty' hsla(266, 32%, 64%, 1),
+  'r' 4 'lilac' hsla(292, 28%, 68%, 1),
+  't' 5 'pink-lavender' hsla(327, 43%, 79%, 1);
+
+:root {
+  --__origin: 'styles/color';
+
+  @each $sid, $sidx, $sname, $hsla in $shades {
+    $fillcolor: $hsla;
+    $fill-highlight: color.change($fillcolor, $lightness: +30%);
+
+    $textcolor: color.change(
+      color.scale($fillcolor, $saturation: -60%),
+      $lightness: math.percentage(0.65 + ($sidx * 0.065))
+    );
+    $text-highlight: color.change($textcolor, $saturation: +30%);
+
+    // named fill + highlight
+    --#{$sname}: #{$fillcolor};
+    --#{$sname}-highlight: #{$fill-highlight};
+
+    // named text + highlight
+    --#{$sname}__text: #{$textcolor};
+    --#{$sname}__text-highlight: #{$text-highlight};
+
+    // identifier fill + highlight
+    --#{$sid}-fill: var(--#{$sname});
+    --#{$sid}-fill-hi: var(--#{$sname}-highlight);
+
+    // identifier text + highlight
+    --#{$sid}-text: var(--#{$sname}__text);
+    --#{$sid}-text-hi: var(--#{$sname}__text-highlight);
+  }
+}
+
+@each $sid, $sidx, $sname, $hsla in $shades {
+  // palette selectors
+  .pal-#{$sid},
+  .pal-#{$sidx},
+  .#{$sname},
+  [data-pal='#{$sid}'],
+  [data-pal='#{$sidx}'] [data-pal='#{$sname}'] {
+    --fill-color: var(--#{$sid}-fill);
+    --text-color: var(--#{$sid}-text);
+    --text-hi: var(--#{$sid}-text-hi);
+  }
+}
+    `}</Highlighter>
   </article>
 )
 
